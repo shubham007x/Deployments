32fd0878553e265ee98e4d9c45662f99787879bb
Shubham Agdari <shubham.agdari@gmail.com>
2025-01-13 14:27:56 +0530

    backend deployment

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/users", userRouter);
-app.listen(8000, async () => {
+app.listen(process.env.PORT, async () => {
   try {
     await connection;
     console.log("DB connected successfully");
